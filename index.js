fdc6b35b3f44206eb4e924f695e6476657fa491d
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-15 16:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === 'hello') {
     return "I can't hear you!";
-    } 
+  } else if (string === 'HELLO') {
+    return "YES INDEED!";
+  } else (string === "I Love you, Grandma") 
+    return "I Love you, too."
 }
