073fcd49a7c1b00c4704e2bc623f6462f596324e
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-15 16:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === 'hello!') {
+  if (string.toLowerCase === 'hello') {
     return "I can't hear you!";
-    } else if (string.toUpperCase === 'HELLO!') {
+    } else if (string.toUpperCase === 'HELLO') {
     return "YES INDEED!";
     } else 
     return "I Love you, too.";
